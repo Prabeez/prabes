8ada7530fd969289a66ffc2c96100648a7735772
Prabeez <141338491+Prabeez@users.noreply.github.com>
2023-08-18 16:15:22 +1000

    uop

--- array/array/array.cs
+++ array/array/array.cs
@@ -7,11 +7,12 @@ namespace SubjectArrayExample
         static void Main(string[] args)
         {
             // Create an array of subjects
-            string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "Toyota" };
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "Honda" };
             Console.WriteLine(cars[0]);
             Console.WriteLine(cars[1]);
             Console.WriteLine(cars[2]);
             Console.WriteLine(cars[3]);
+            Console.WriteLine(cars[4]);
 
         }
     }
